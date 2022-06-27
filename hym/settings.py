4bfe42e9b41419ffc2f1ef82f9590d688ab7469d
david <david.cesarmc@outlook.es>
2022-06-26 19:53:44 -0500

    conf de template para evitar errores

--- hym/settings.py
+++ hym/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'hym.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['D:/Proyectos/hym/hym/templates'],
+        'DIRS': [BASE_DIR / 'hym/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
